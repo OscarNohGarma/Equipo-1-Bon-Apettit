ff3c6a39ce294da71d7854971711cc1276fc1bb8
Carlos Quintal <7647@itescam.edu.mx>
2024-09-09 00:24:47 -0600

    Creación del componente Inicio

--- el-abuelo-angular/src/app/app.component.ts
+++ el-abuelo-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
