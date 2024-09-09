4087e71dc25d857b5fd910e47bdec64bee564c03
Oscar Garma <spongetcrazy@gmail.com>
2024-09-09 10:08:22 -0600

    Add routing to navbar

--- el-abuelo-angular/src/app/app.routes.ts
+++ el-abuelo-angular/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { GaleriaInstalacionesComponent } from './components/galeria-instalaciones/galeria-instalaciones.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'menu', component: MenuComponent },
   { path: 'Galeria', component: GaleriaInstalacionesComponent },
-
 ];
