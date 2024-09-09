b4c5f3aab280368beb296b0b26ddee79f4104764
WoolyOlvel <pucalan20@gmail.com>
2024-09-09 01:53:14 -0600

    Mostrar fotos de las instalaciones_1

---
+++ el-abuelo-angular/src/app/components/galeria-instalaciones/galeria-instalaciones.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+@Component({
+  selector: 'app-galeria-instalaciones',
+  standalone: true,
+  imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './galeria-instalaciones.component.html',
+  styleUrl: './galeria-instalaciones.component.scss'
+})
+export class GaleriaInstalacionesComponent {
+
+}
--- el-abuelo-angular/src/app/components/navbar/navbar.component.ts
+++ el-abuelo-angular/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { GaleriaInstalacionesComponent } from '../galeria-instalaciones/galeria-instalaciones.component';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, GaleriaInstalacionesComponent, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
