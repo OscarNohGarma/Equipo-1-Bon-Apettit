c8368fa89a75b8636f5ff91e57af5744838e4c73
WoolyOlvel <pucalan20@gmail.com>
2024-09-21 12:46:17 -0600

    Updated Gallery Installations Fix

--- el-abuelo-angular/src/app/components/galeria-instalaciones/galeria-instalaciones.component.ts
+++ el-abuelo-angular/src/app/components/galeria-instalaciones/galeria-instalaciones.component.ts
@@ -13,33 +13,26 @@ export class GaleriaInstalacionesComponent  {
   modelos = [
     {
       nombre: 'Parador Star',
-      descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio vel dolor voluptates, asperiores quod'
-            +'laudantium repellendus? Repudiandae exercitationem ex magni! Veritatis consectetur cupiditate,'+
-            'voluptatems quae repellat perspiciatis assumenda explicabo.',
+      descripcion: 'Un espacio acogedor para disfrutar de los mejores momentos del día. El Parador Star te ofrece una atmósfera cálida y relajante, perfecta para saborear tu café favorito o desconectarte con un buen libro. Descubre el encanto de este rincón donde el tiempo parece detenerse.',
       imagen: '../../../assets/galeriaInstalaciones/PCentral.jpg',
-      clase: 'basico'
+      clase: 'elite'
     },
     {
       nombre: 'Parador Fotográfico',
-      descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio vel dolor voluptates, asperiores quod'
-            +'laudantium repellendus? Repudiandae exercitationem ex magni! Veritatis consectetur cupiditate,'+
-            'voluptatem quae repellat perspiciatis assumenda explicabo.',
+      descripcion: 'Captura recuerdos inolvidables en nuestro Parador Fotográfico, donde cada rincón está diseñado para enamorar a la cámara. Ya sea que busques una foto casual o una toma profesional, este espacio ofrece el escenario perfecto para compartir en redes o guardar en tu álbum personal.',
       imagen: '../../../assets/galeriaInstalaciones/PFotografico.jpg',
-      clase: 'premier'
+      clase: 'premier',
+
     },
     {
       nombre: 'Comedor Interno',
-      descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio vel dolor voluptates, asperiores quod'
-            +'laudantium repellendus? Repudiandae exercitationem ex magni! Veritatis consectetur cupiditate,'+
-            'voluptatem quae repellat perspiciatis assumenda explicabo.',
+      descripcion: 'Vive una experiencia gastronómica única en nuestro Comedor Interno, un lugar exclusivo donde el confort y el estilo se unen. Disfruta de platos exquisitos en un entorno moderno y elegante que te invita a quedarte un rato más. Ideal para reuniones o momentos especiales.',
       imagen: '../../../assets/galeriaInstalaciones/PComedor.jpg',
       clase: 'elite'
     },
     {
       nombre: 'Sala Parejas',
-      descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum odio vel dolor voluptates, asperiores quod'
-            +'laudantium repellendus? Repudiandae exercitationem ex magni! Veritatis consectetur cupiditate,'+
-            'voluptatem quae repellat perspiciatis assumenda explicabo.',
+      descripcion: 'La Sala Parejas es el refugio perfecto para compartir momentos íntimos y románticos. Con un ambiente íntimo y un diseño cuidadosamente pensado, este espacio está creado para que disfrutes de la compañía de esa persona especial, lejos de las distracciones.',
       imagen: '../../../assets/galeriaInstalaciones/Pprivado.jpg',
       clase: 'premier'
     }
