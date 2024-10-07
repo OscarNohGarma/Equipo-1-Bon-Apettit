071830de1f90fbbacb286ac45ed43a5efa379628
Oscar Garma <spongetcrazy@gmail.com>
2024-10-06 22:33:49 -0600

    Add menu on admin

--- el-abuelo-angular/src/app/admin/admin.routes.ts
+++ el-abuelo-angular/src/app/admin/admin.routes.ts
@@ -1,11 +1,17 @@
 // feature.routes.ts
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { InicioAdminComponent } from './inicio-admin/inicio-admin.component';
+import { MenuAdminComponent } from './menu-admin/menu-admin.component';
 
 export const adminRoutes: Routes = [
   {
     path: '', // Ruta para cargar el componente FeatureComponent
     title: 'Administrador - El abuelo',
-    component: DashboardComponent,
+    component: InicioAdminComponent,
+  },
+  {
+    path: 'menu', // Ruta para cargar el componente FeatureComponent
+    title: 'Administrador - El abuelo',
+    component: MenuAdminComponent,
   },
 ];
---
+++ el-abuelo-angular/src/app/admin/menu-admin/menu-admin.component.ts
@@ -0,0 +1,68 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../../core/services/menu.service';
+import { MenuProduct } from '../../core/models/menuProduct';
+
+@Component({
+  selector: 'app-menu-admin',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
+  templateUrl: './menu-admin.component.html',
+  styleUrl: './menu-admin.component.scss',
+  providers: [MenuService],
+})
+export class MenuAdminComponent implements OnInit {
+  selectedCategory: string = 'TODOS'; // Categoría por defecto
+  isMenuOpen: boolean = false; // Para controlar la visibilidad del menú desplegable
+  showOrder: boolean = false; // Controla la visibilidad de la orden
+  menuItems: MenuProduct[] = [];
+  expandedImage: string | null = null; // Controla la imagen expandida
+
+  constructor(private menuService: MenuService) {} // Inyectar el servicio
+
+  ngOnInit(): void {
+    // this.http.get('http://localhost:3000/firebase/menu/getmenu').subscribe(
+    //   (data) => {
+    //     console.log(data);
+    //   },
+    //   (error) => {
+    //     console.error(error);
+    //   }
+    // );
+    this.loadMenu();
+  }
+  loadMenu(): void {
+    this.menuService.getMenu().subscribe((data) => {
+      this.menuItems = data;
+      console.log(this.menuItems);
+    });
+  }
+
+  get filteredProducts(): MenuProduct[] {
+    if (this.selectedCategory === 'TODOS') {
+      return this.menuItems;
+    }
+    return this.menuItems.filter(
+      (product) => product.category === this.selectedCategory
+    );
+  }
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+    this.isMenuOpen = false; // Cierra el menú después de seleccionar una opción
+  }
+
+  // Función para abrir la imagen
+  openImage(imageUrl: string) {
+    this.expandedImage = imageUrl;
+  }
+
+  // Función para cerrar la imagen
+  closeImage() {
+    this.expandedImage = null;
+  }
+  editarProducto(product: MenuProduct) {}
+}
