08f81bf7a3bf31216b5388d93808b489cf6aeb21
Oscar Garma <spongetcrazy@gmail.com>
2024-10-17 13:19:38 -0600

    Fix upload images

--- el-abuelo-angular/src/app/core/services/upload.service.ts
+++ el-abuelo-angular/src/app/core/services/upload.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class UploadService {
-  private apiUrl = 'http://localhost:3000/firebase/upload';
+  private apiUrl = 'http://localhost:3000/upload';
 
   constructor(private http: HttpClient) {}
 
@@ -16,7 +16,7 @@ export class UploadService {
     formData.append('file', file); // Clave 'file', como lo haces en Postman
 
     // Realiza la petición POST para subir la imagen
-    return this.http.post(`${this.apiUrl}/addimg`, formData, {
+    return this.http.post(`${this.apiUrl}`, formData, {
       reportProgress: true,
       observe: 'events',
     });
@@ -24,11 +24,11 @@ export class UploadService {
 
   // Método para obtener una imagen por nombre de archivo
   getImage(fileName: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/getimg/${fileName}`);
+    return this.http.get(`${this.apiUrl}/${fileName}`);
   }
 
   // Método para eliminar una imagen por nombre de archivo
   deleteImage(fileName: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/deleteimg/${fileName}`);
+    return this.http.delete(`${this.apiUrl}/${fileName}`);
   }
 }
